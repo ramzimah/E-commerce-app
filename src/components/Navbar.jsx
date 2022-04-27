848515d17d6ba698565bd597a0c5627d5d9377e1
ramzimah <ramzimahnoune@gmail.com>
2022-04-27 02:42:16 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+`;
+
+const Navbar = () => {
+  return <Container>Navbar</Container>;
+};
+
+export default Navbar;
